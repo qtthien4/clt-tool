7eaf84ca44e71c88de97a029f79bfa38e7ba91bc
thien vo <thien.vo.tpv@one-line.com>
2025-03-19 23:28:25 +0700

    update vercel v2

--- api/index.js
+++ api/index.js
@@ -28,6 +28,6 @@ app.get("/check-login", async (req, res) => {
   res.status(200).send("success!");
 });
 
-app.listen(3001, () => console.log("Server ready on port 3001."));
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 exports.myFunction = app;
